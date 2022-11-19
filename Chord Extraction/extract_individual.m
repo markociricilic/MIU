84dee736b093331a66dc85b57236cbb6d9fb7d59
tzhou <85449931+tangiazhou@users.noreply.github.com>
2022-11-19 15:00:45 -0500

    Change from stereo to mono

--- Chord Extraction/extract_individual.m
+++ Chord Extraction/extract_individual.m
@@ -1,60 +1,59 @@
 % G Major Chord
 [gmajor, Fs] = audioread("gmajor.wav");
+single_gmajor = gmajor(:,1); % Single channel
 
-N = length(gmajor); % Number of points on the FFT
+N = length(single_gmajor); % Number of points on the FFT
 f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(gmajor); % FFT of G Major chord
+X = fft(single_gmajor); % FFT of G Major chord
 
 figure
 plot(f, abs(X));
+xlim([0 2000])
 title("FFT of G Major Chord")
 xlabel("Frequency (Hz)")
 ylabel("real(fft(gmajor))")
 
-sound(gmajor, Fs)
-
 % F Major Chord
 [fmajor, Fs] = audioread("fmajor.wav");
+single_fmajor = fmajor(:,1); % Single channel
 
-N = length(fmajor); % Number of points on the FFT
+N = length(single_fmajor); % Number of points on the FFT
 f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(fmajor); % FFT
+X = fft(single_fmajor); % FFT of G Major chord
 
 figure
 plot(f, abs(X));
+xlim([0 2000])
 title("FFT of F Major Chord")
 xlabel("Frequency (Hz)")
 ylabel("real(fft(fmajor))")
 
-sound(fmajor, Fs)
-
 % D Minor Chord
 [dminor, Fs] = audioread("dminor.wav");
+single_dminor = dminor(:,1); % Single channel
 
-N = length(dminor); % Number of points for the FFT
+N = length(single_dminor); % Number of points on the FFT
 f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(dminor); % FFT
+X = fft(single_dminor); % FFT of G Major chord
 
 figure
 plot(f, abs(X));
+xlim([0 2000])
 title("FFT of D Minor Chord")
 xlabel("Frequency (Hz)")
 ylabel("real(fft(dminor))")
 
-sound(dminor, Fs)
-
 % C Major Chord
 [cmajor, Fs] = audioread("cmajor.wav");
+single_cmajor = cmajor(:,1); % Single channel
 
-N = length(cmajor); % Number of points for the FFT
+N = length(single_cmajor); % Number of points on the FFT
 f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(cmajor); % FFT
+X = fft(single_cmajor); % FFT of G Major chord
 
 figure
 plot(f, abs(X));
+xlim([0 2000])
 title("FFT of C Major Chord")
 xlabel("Frequency (Hz)")
 ylabel("real(fft(cmajor))")
-
-sound(cmajor, Fs)
-
