756284833f2559b01b0546c747020eb32aca6763
Marko Ćirić <markociricilic@gmail.com>
2022-11-23 13:45:27 -0500

    Tangia V.2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn-v0_8')
 
-# Load data from G Major Chord wav file
+# Load data from Full Progression wav file
 Fs, fullprogression = wavfile.read('Chord Extraction/fullprogression.wav')
 single_fullprogression = fullprogression[:,1] # Single channel
 split_signal = np.split(single_fullprogression,5) # Split 
@@ -35,50 +35,3 @@ plt.title('FFT of Chord Progression')
 plt.xlim((0, 2000))
 plt.grid()
 # %%
-
-""""
-% F Major Chord
-[fmajor, Fs] = audioread("fmajor.wav");
-single_fmajor = fmajor(:,1); % Single channel
-
-N = length(single_fmajor); % Number of points on the FFT
-f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(single_fmajor); % FFT of G Major chord
-
-figure
-plot(f, abs(X));
-xlim([0 2000])
-title("FFT of F Major Chord")
-xlabel("Frequency (Hz)")
-ylabel("real(fft(fmajor))")
-
-% D Minor Chord
-[dminor, Fs] = audioread("dminor.wav");
-single_dminor = dminor(:,1); % Single channel
-
-N = length(single_dminor); % Number of points on the FFT
-f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(single_dminor); % FFT of G Major chord
-
-figure
-plot(f, abs(X));
-xlim([0 2000])
-title("FFT of D Minor Chord")
-xlabel("Frequency (Hz)")
-ylabel("real(fft(dminor))")
-
-% C Major Chord
-[cmajor, Fs] = audioread("cmajor.wav");
-single_cmajor = cmajor(:,1); % Single channel
-
-N = length(single_cmajor); % Number of points on the FFT
-f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(single_cmajor); % FFT of G Major chord
-
-figure
-plot(f, abs(X));
-xlim([0 2000])
-title("FFT of C Major Chord")
-xlabel("Frequency (Hz)")
-ylabel("real(fft(cmajor))")
-"""
