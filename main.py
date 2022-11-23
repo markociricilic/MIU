c2183d8d0f48145badec7061c6a5e34cf599aa95
Joey Zhang <zjoey049@gmail.com>
2022-11-23 12:24:29 -0500

    Current Code for splitting up full Progression into 5 equal segments

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ plt.style.use('seaborn-v0_8')
 
 # Load data from G Major Chord wav file
 Fs, fullprogression = wavfile.read('Chord Extraction/fullprogression.wav')
-single_fullprogression = fullprogression[:,1]; # Single channel
+single_fullprogression = fullprogression[:,1] # Single channel
+split_signal = np.split(single_fullprogression,5)
+#split_signal[0] --> C major chord hopefully
+#split_signal[1] --> F major chord hoepfully .....
+duration_of_signal= len(split_signal[1])
+print("length of signal: ")
+print(duration_of_signal)
 
 #FFT
-N = np.arange(single_fullprogression.shape[0])    # Number of points on the FFT
+N = np.arange(split_signal[1].shape[0])    # Number of points on the FFT
 freq = np.fft.fftfreq(N.shape[-1])*Fs             # Frequency
-FP = np.fft.fft(single_fullprogression)           # FFT of G Major chord
+FP = np.fft.fft(split_signal[1])           # FFT of G Major chord
 
 # Plot spectrum
 plt.plot(freq, abs(FP))
