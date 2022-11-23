8a7df44d82f422ca404d5e8bd831ff3cb3289faf
Marko Ćirić <markociricilic@gmail.com>
2022-11-23 13:42:49 -0500

    Tangia's turn

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn-v0_8')
 
-# Load data from G Major Chord wav file
+# Load data from Chord Progression wav file
 Fs, fullprogression = wavfile.read('Chord Extraction/fullprogression.wav')
 single_fullprogression = fullprogression[:,1] # Single channel
 split_signal = np.split(single_fullprogression,5)
@@ -22,11 +22,11 @@ print(duration_of_signal)
 
 #FFT
 N = np.arange(split_signal[1].shape[0])    # Number of points on the FFT
-freq = np.fft.fftfreq(N.shape[-1])*Fs             # Frequency
+freq = np.fft.fftfreq(N.shape[-1])*Fs      # Frequency
 FP = np.fft.fft(split_signal[1])           # FFT of G Major chord
 
-peaks = find_peaks(abs(FP.real))
-peaks_sorted = np.sort(peaks)
+peaks, _ = find_peaks(abs(FP))
+peaks_sorted = sorted(zip(peaks, freq), reverse=True)[:5]
 print(peaks_sorted)
 
 # Plot spectrum
@@ -37,50 +37,3 @@ plt.title('FFT of Chord Progression')
 plt.xlim((0, 2000))
 plt.grid()
 # %%
-
-""""
-% F Major Chord
-[fmajor, Fs] = audioread("fmajor.wav");
-single_fmajor = fmajor(:,1); % Single channel
-
-N = length(single_fmajor); % Number of points on the FFT
-f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(single_fmajor); % FFT of G Major chord
-
-figure
-plot(f, abs(X));
-xlim([0 2000])
-title("FFT of F Major Chord")
-xlabel("Frequency (Hz)")
-ylabel("real(fft(fmajor))")
-
-% D Minor Chord
-[dminor, Fs] = audioread("dminor.wav");
-single_dminor = dminor(:,1); % Single channel
-
-N = length(single_dminor); % Number of points on the FFT
-f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(single_dminor); % FFT of G Major chord
-
-figure
-plot(f, abs(X));
-xlim([0 2000])
-title("FFT of D Minor Chord")
-xlabel("Frequency (Hz)")
-ylabel("real(fft(dminor))")
-
-% C Major Chord
-[cmajor, Fs] = audioread("cmajor.wav");
-single_cmajor = cmajor(:,1); % Single channel
-
-N = length(single_cmajor); % Number of points on the FFT
-f = (0:(N-1))*(Fs/N); % Frequency
-X = fft(single_cmajor); % FFT of G Major chord
-
-figure
-plot(f, abs(X));
-xlim([0 2000])
-title("FFT of C Major Chord")
-xlabel("Frequency (Hz)")
-ylabel("real(fft(cmajor))")
-"""
