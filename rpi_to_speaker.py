fc7b9bd14a36fb9751f208de42ac246bceda911d
Marko Ćirić <markociricilic@gmail.com>
2022-11-28 17:05:27 -0500

    Delete rpi_to_speaker.py

--- rpi_to_speaker.py
+++
@@ -1,9 +0,0 @@
-# Python libraries
-import pygame
-
-# plays the test.wav file
-pygame.mixer.init()
-pygame.mixer.music.load("test.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
