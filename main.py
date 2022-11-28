8c19c3730c98d23b768d90d7bb9427da6d53217f
Tangia Zhou <tangia.zhou@gmail.com>
2022-11-28 16:45:12 -0500

    Whole file

--- main.py
+++ main.py
@@ -171,4 +171,12 @@ plt.xlim([0,1])
 
 complete = main_melody_vector
 wavfile.write("test.wav",Fs,complete)
+
+# Concatenate two 
+single_fullprogression 
+complete
+ 
+generated = single_fullprogression + complete
+wavfile.write("randomized.wav", Fs, generated)
+
 # %%
